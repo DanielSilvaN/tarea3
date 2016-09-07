5b67deb0162249682174307d49ba6b180a48a80c
Daniel Silva Navia <Daniel Silva Navia>
2016-09-07 16:00:36 -0300

    segundo commit

--- Tarea_3/Tarea_3/Class1.cs
+++ Tarea_3/Tarea_3/Class1.cs
@@ -10,7 +10,22 @@ namespace Tarea_3
     {
         var phrase = "lalalalalalalalalalalalalalalalalalalalalalalalalalalalalala";
         var manyPhrases = new StringBuilder();
-for (var i = 0; i< 10000; i++)
+for (var i = 0;
+
+        public object Phrase
+        {
+            get
+            {
+                return phrase;
+            }
+
+            set
+            {
+                phrase = value;
+            }
+        }
+
+        i< 10000; i++)
 {
     manyPhrases.Append(phrase);
 }
